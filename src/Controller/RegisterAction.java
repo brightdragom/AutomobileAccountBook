58e5e4d870c02d3ac662bcc9b6913d4af52281a5
yonghuna <50226372+yonghuna@users.noreply.github.com>
2020-06-15 14:28:16 +0900

    회원가입 수정 경로 수정

--- src/Controller/RegisterAction.java
+++ src/Controller/RegisterAction.java
@@ -10,43 +10,50 @@ import javax.servlet.http.HttpServletResponse;
 
 import DAO.DBConnection;
 import DAO.DBConnection.*;
+
 @WebServlet("/RegisterAction")
-public class RegisterAction extends HttpServlet{
+public class RegisterAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
 		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
+		String pw1 = request.getParameter("pw1");
+		String pw2 = request.getParameter("pw2");
 		String phone = request.getParameter("phone");
 		String email = request.getParameter("email");
 		String part = request.getParameter("part");
 		String addr = request.getParameter("addr");
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
-		if(id == null || id.equals("") || pw == null || pw.equals("" ) || phone == null || phone.equals("") ||
-				email == null || email.equals("") || part == null || part.equals("") || addr == null || addr.equals("")
-				|| name == null || name.equals("") ||  gender == null || gender.equals("")){
+		if (id == null || id.equals("") || pw1 == null || pw1.equals("") || phone == null || phone.equals("")
+				|| email == null || email.equals("") || part == null || part.equals("") || addr == null
+				|| addr.equals("") || name == null || name.equals("") || gender == null || gender.equals("")) {
 			request.getSession().setAttribute("messageType", "오류");
-			request.getSession().setAttribute("messageType", "모든 내용을 입력 하시오");
-			response.sendRedirect("main/Register.jsp");
+			request.getSession().setAttribute("messageContent", "모든 내용을 입력 하시오");
+			response.sendRedirect("login/Register.jsp");
 			return;
 		}
-		int result = new DBConnection().register(name, id, pw, phone, email, part, addr, gender);
+		if (!pw1.equals(pw2)) {
+			request.getSession().setAttribute("messageType", "오류");
+			request.getSession().setAttribute("messageContent", "비밀번호가 서로 일치하지 않습니다.");
+			response.sendRedirect("login/Register.jsp");
+			return;
+		}
+		int result = new DBConnection().register(name, id, pw1, phone, email, part, addr, gender);
 		if (result == 1) {
 			request.getSession().setAttribute("messageType", "성공");
-			request.getSession().setAttribute("messageType", "회원가입 성공");
-			response.sendRedirect("main/Register.jsp");
+			request.getSession().setAttribute("messageContent", "회원가입 성공");
+			response.sendRedirect("login/Register.jsp");
 			return;
 		} else {
 			request.getSession().setAttribute("messageType", "오류 메세지");
-			request.getSession().setAttribute("messageType", "이미 존재 합니다.");
-			response.sendRedirect("main/Register.jsp");
-
+			request.getSession().setAttribute("messageContent", "이미 존재 합니다.");
+			response.sendRedirect("login/Register.jsp");
 			return;
 		}
 	}
-	
-	
+
 }
