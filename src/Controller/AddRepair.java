7b4c377c16b3d3212a08a005a8c48a15b8083d13
brightdragom <brightdragom@gmail.com>
2020-07-10 11:31:30 +0900

    Add Git Test code

--- src/Controller/AddRepair.java
+++ src/Controller/AddRepair.java
@@ -26,6 +26,7 @@ public class AddRepair extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Tests");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
